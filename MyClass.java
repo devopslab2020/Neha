b42d34380e0cb223ea9a9c48a16d3dededfb7a25
devopslab2020 <devopslab2020@gmail.com>
2020-01-30 05:27:07 +0530

    MyClass.java1

--- MyClass.java
+++ MyClass.java
@@ -1,6 +1,13 @@
 public class MyClass {
+
+  static void gameids(String fname){
+	System.out.println(fname + "refernces");
+}
+	
   public static void main(String[] args) {
-    System.out.println("Hello World");
+ 	gameids("Alps");   
+  	gameids("10Neha01");
+	gameids("Nirstroke");
   }
 }
 
